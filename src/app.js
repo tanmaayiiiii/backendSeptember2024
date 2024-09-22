58cd920349cadf34b4ad30960573c60c35e4dfad
Tanmayi Ghadge <tanmayighadge@gmail.com>
2024-09-21 20:51:43 -0700

    added register controller, user can be registered

--- src/app.js
+++ src/app.js
@@ -13,7 +13,13 @@ app.use(cors({
 app.use(express.json({limit: "16kb"}))//accepting json
 app.use(express.urlencoded({extended:true,limit:"16kb"}))//to get data from url
 app.use(express.static("public"))//to store pdfs, images,etc
-app.use(cookieParser)
+app.use(cookieParser())
+
+//routes import
+import userRouter from './routes/user.routes.js'
+
+//routes declaration
+app.use("/api/v1/users", userRouter)
 
 
 export { app }
